61c71bcdfb5acb64b1cffa9d0b918ddae9f16168
jkotrady <kotrady.johnny@gmail.com>
2016-11-14 10:04:28 +0100

    version exception

---
+++ war/src/main/java/io/lumeer/engine/exception/VersionUpdateConflictException.java
@@ -0,0 +1,41 @@
+/*
+ * -----------------------------------------------------------------------\
+ * Lumeer
+ *  
+ * Copyright (C) 2016 - 2017 the original author or authors.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -----------------------------------------------------------------------/
+ */
+package io.lumeer.engine.exception;
+
+import io.lumeer.engine.DbException;
+
+/**
+ * @author <a href="mailto:kotrady.johnny@gmail.com">Jan Kotrady</a>
+ */
+public class VersionUpdateConflictException extends DbException {
+
+   public VersionUpdateConflictException(final String message) {
+      super(message);
+   }
+
+   public VersionUpdateConflictException(final String message, final Throwable cause) {
+      super(message, cause);
+   }
+
+   public VersionUpdateConflictException(final Throwable cause) {
+      super(cause);
+   }
+
+}
