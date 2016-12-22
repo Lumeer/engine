701d3c92513db1689a7d8e9aa413d25507ca1096
Martin Večeřa <marvenec@gmail.com>
2016-12-22 23:10:00 +0100

    batch service test

--- war/src/test/java/io/lumeer/engine/rest/BatchServiceTest.java
+++ war/src/test/java/io/lumeer/engine/rest/BatchServiceTest.java
@@ -64,6 +64,12 @@ public class BatchServiceTest extends Arquillian {
 
    @Test
    public void testSplitBatch() throws Exception {
+      long t = System.currentTimeMillis();
+      int i = 1;
+
+      System.out.println(i++ + " " + (System.currentTimeMillis() - t));
+      t = System.currentTimeMillis();
+
       final Client client = ClientBuilder.newBuilder().build();
       // the path prefix '/lumeer-engine/' does not work in test classes
       Response response = client.target(TARGET_URI)
@@ -76,6 +82,9 @@ public class BatchServiceTest extends Arquillian {
       response.close();
       Assert.assertTrue(status == 404 || status == 204);
 
+      System.out.println(i++ + " " + (System.currentTimeMillis() - t));
+      t = System.currentTimeMillis();
+
       response = client.target(TARGET_URI)
                        .path(PATH_PREFIX + "collections/" + COLLECTION_NAME)
                        .request(MediaType.APPLICATION_JSON_TYPE)
@@ -84,6 +93,9 @@ public class BatchServiceTest extends Arquillian {
       status = response.getStatus();
       response.close();
       Assert.assertEquals(status, 200);
+
+      System.out.println(i++ + " " + (System.currentTimeMillis() - t));
+      t = System.currentTimeMillis();
 
       response = client.target(TARGET_URI)
             .path(PATH_PREFIX + "collections/" + COLLECTION_NAME + "/documents/")
@@ -97,6 +109,9 @@ public class BatchServiceTest extends Arquillian {
 
       Assert.assertTrue(documentId != null && documentId.length() > 5);
 
+      System.out.println(i++ + " " + (System.currentTimeMillis() - t));
+      t = System.currentTimeMillis();
+
       response = client.target(TARGET_URI)
             .path(PATH_PREFIX + "collections/" + COLLECTION_NAME + "/documents/")
             .request(MediaType.APPLICATION_JSON_TYPE)
@@ -104,6 +119,10 @@ public class BatchServiceTest extends Arquillian {
             .buildPost(Entity.json(new DataDocument("attr1", 5).append("attr2", "hello, how are, you 2")))
             .invoke();
       response.close();
+
+      System.out.println(i++ + " " + (System.currentTimeMillis() - t));
+      t = System.currentTimeMillis();
+
       response = client.target(TARGET_URI)
             .path(PATH_PREFIX + "collections/" + COLLECTION_NAME + "/documents/")
             .request(MediaType.APPLICATION_JSON_TYPE)
@@ -112,12 +131,18 @@ public class BatchServiceTest extends Arquillian {
             .invoke();
       response.close();
 
+      System.out.println(i++ + " " + (System.currentTimeMillis() - t));
+      t = System.currentTimeMillis();
+
       response = client.target(TARGET_URI)
             .path(PATH_PREFIX + "batch/split/")
             .request(MediaType.APPLICATION_JSON_TYPE)
             .buildPost(Entity.json(new SplitBatch(COLLECTION_NAME, "attr2", ",", true, Arrays.asList("attr2a", "attr2b", "attr2c", "attr2d"), false)))
             .invoke();
       response.close();
+
+      System.out.println(i++ + " " + (System.currentTimeMillis() - t));
+      t = System.currentTimeMillis();
 
       response = client.target(TARGET_URI)
             .path(PATH_PREFIX + "query/")
@@ -128,10 +153,15 @@ public class BatchServiceTest extends Arquillian {
       final List<LinkedHashMap> result = response.readEntity(List.class);
       response.close();
 
-      result.forEach(System.out::println);
+      System.out.println(i++ + " " + (System.currentTimeMillis() - t));
+      t = System.currentTimeMillis();
 
-
-      //Thread.sleep(5 * 60 * 1000L);
+      result.forEach(data -> {
+         Assert.assertFalse(data.containsKey("attr2"));
+         Assert.assertEquals(data.get("attr2a"), "hello");
+         Assert.assertEquals(data.get("attr2b"), "how are");
+         Assert.assertFalse(data.containsKey("attr2d"));
+      });
    }
 
 }
