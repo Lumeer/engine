7428464fdb79599589cc519575495998258112fb
Matej Perejda <mat.per.vt@gmail.com>
2017-02-19 20:40:47 +0100

    fixed bug in DocumentServiceTest

--- war/src/test/java/io/lumeer/engine/rest/DocumentServiceTest.java
+++ war/src/test/java/io/lumeer/engine/rest/DocumentServiceTest.java
@@ -102,9 +102,11 @@ public class DocumentServiceTest extends Arquillian {
       DataDocument updatedDocument = new DataDocument();
       updatedDocument.put("_id", documentId);
       updatedDocument.put("name", "updatedDocument");
-      Response response3 = client.target(TARGET_URI).path(setPathPrefix(COLLECTION_CREATE_READ_UPDATE_AND_DROP_DOCUMENT + "/update")).request().buildPut(Entity.json(updatedDocument)).invoke();
+      Response response3 = client.target(TARGET_URI).path(setPathPrefix(COLLECTION_CREATE_READ_UPDATE_AND_DROP_DOCUMENT) + "update/").request().buildPut(Entity.json(updatedDocument)).invoke();
+
       Assert.assertTrue(response3.getStatus() == Response.Status.NO_CONTENT.getStatusCode()
             && documentFacade.readDocument(getInternalName(COLLECTION_CREATE_READ_UPDATE_AND_DROP_DOCUMENT), documentId).getString("name").equals("updatedDocument"));
+
       response3.close();
 
       // 204 - drop the given document by its id
