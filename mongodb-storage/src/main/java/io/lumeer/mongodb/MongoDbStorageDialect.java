ceea62f449928fec1cdc6bf0eeae9c0acb722028
Martin Večeřa <marvenec@gmail.com>
2017-03-30 01:02:58 +0200

    Updated storage to use data filter #LMR-109

--- mongodb-storage/src/main/java/io/lumeer/mongodb/MongoDbStorageDialect.java
+++ mongodb-storage/src/main/java/io/lumeer/mongodb/MongoDbStorageDialect.java
@@ -21,10 +21,10 @@ package io.lumeer.mongodb;
 
 import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
-import static io.lumeer.mongodb.MongoUtils.convertBsonToJson;
 
 import io.lumeer.engine.api.LumeerConst;
 import io.lumeer.engine.api.data.DataDocument;
+import io.lumeer.engine.api.data.DataFilter;
 import io.lumeer.engine.api.data.DataStorageDialect;
 
 import org.bson.conversions.Bson;
@@ -41,32 +41,36 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class MongoDbStorageDialect implements DataStorageDialect {
 
+   private DataFilter createFilter(final Bson filter) {
+      return new MongoDbDataFilter(filter);
+   }
+
    @Override
    public DataDocument updateCollectionAttributeCountQuery(final String metadataCollectionName, final String attributeName) {
       return new DataDocument(); // TODO based on new query in CollectionMetadataFacade
    }
 
    @Override
-   public String linkingFromTablesColNameFilter(final String collectionName, final String role) {
+   public DataFilter linkingFromTablesColNameFilter(final String collectionName, final String role) {
       Bson filterRaw = role == null || role.isEmpty()
             ? eq(LumeerConst.Linking.MainTable.ATTR_COL_NAME, collectionName)
             : and(eq(LumeerConst.Linking.MainTable.ATTR_COL_NAME, collectionName),
             eq(LumeerConst.Linking.MainTable.ATTR_ROLE, role));
-      return convertBsonToJson(filterRaw);
+      return createFilter(filterRaw);
    }
 
    @Override
-   public String linkingFromTablesFilter(final String firstCollectionName, final String role, final LumeerConst.Linking.LinkDirection linkDirection) {
+   public DataFilter linkingFromTablesFilter(final String firstCollectionName, final String role, final LumeerConst.Linking.LinkDirection linkDirection) {
       String collParam = linkDirection == LumeerConst.Linking.LinkDirection.FROM ? LumeerConst.Linking.MainTable.ATTR_FROM_COLLECTION : LumeerConst.Linking.MainTable.ATTR_TO_COLLECTION;
       Bson filterRaw = role == null || role.isEmpty()
             ? eq(collParam, firstCollectionName)
             : and(eq(collParam, firstCollectionName),
             eq(LumeerConst.Linking.MainTable.ATTR_ROLE, role));
-      return convertBsonToJson(filterRaw);
+      return createFilter(filterRaw);
    }
 
    @Override
-   public String linkingFromToTablesFilter(final String firstCollectionName, final String secondCollectionName, final String role, final LumeerConst.Linking.LinkDirection linkDirection) {
+   public DataFilter linkingFromToTablesFilter(final String firstCollectionName, final String secondCollectionName, final String role, final LumeerConst.Linking.LinkDirection linkDirection) {
       String fromCollectionName;
       String toCollectionName;
       if (linkDirection == LumeerConst.Linking.LinkDirection.FROM) {
@@ -82,53 +86,53 @@ public class MongoDbStorageDialect implements DataStorageDialect {
             : and(eq(LumeerConst.Linking.MainTable.ATTR_FROM_COLLECTION, fromCollectionName),
             eq(LumeerConst.Linking.MainTable.ATTR_TO_COLLECTION, toCollectionName),
             eq(LumeerConst.Linking.MainTable.ATTR_ROLE, role));
-      return convertBsonToJson(filterRaw);
+      return createFilter(filterRaw);
    }
 
    @Override
-   public String linkingFromToDocumentFilter(final String fromId, final String toId, final LumeerConst.Linking.LinkDirection linkDirection) {
+   public DataFilter linkingFromToDocumentFilter(final String fromId, final String toId, final LumeerConst.Linking.LinkDirection linkDirection) {
       Bson filterRaw = linkDirection == LumeerConst.Linking.LinkDirection.FROM
             ? and(eq(LumeerConst.Linking.LinkingTable.ATTR_FROM_ID, fromId),
             eq(LumeerConst.Linking.LinkingTable.ATTR_TO_ID, toId))
             : and(eq(LumeerConst.Linking.LinkingTable.ATTR_FROM_ID, toId),
             eq(LumeerConst.Linking.LinkingTable.ATTR_TO_ID, fromId)
       );
-      return convertBsonToJson(filterRaw);
+      return createFilter(filterRaw);
    }
 
    @Override
-   public String linkingFromDocumentFilter(final String fromId, final LumeerConst.Linking.LinkDirection linkDirection) {
+   public DataFilter linkingFromDocumentFilter(final String fromId, final LumeerConst.Linking.LinkDirection linkDirection) {
       Bson filterRaw = linkDirection == LumeerConst.Linking.LinkDirection.FROM
             ? eq(LumeerConst.Linking.LinkingTable.ATTR_FROM_ID, fromId)
             : eq(LumeerConst.Linking.LinkingTable.ATTR_TO_ID, fromId);
-      return convertBsonToJson(filterRaw);
+      return createFilter(filterRaw);
    }
 
    @Override
-   public String fieldValueFilter(final String fieldName, final Object value) {
-      return convertBsonToJson(eq(fieldName, value));
+   public DataFilter fieldValueFilter(final String fieldName, final Object value) {
+      return createFilter(eq(fieldName, value));
    }
 
    @Override
-   public String documentNestedIdFilter(final String documentId) {
+   public DataFilter documentNestedIdFilter(final String documentId) {
       return fieldValueFilter(concatFields(LumeerConst.Document.ID, LumeerConst.Document.ID), new ObjectId(documentId));
    }
 
    @Override
-   public String documentNestedIdFilterWithVersion(final String documentId, final int version) {
-      return convertBsonToJson(eq(LumeerConst.Document.ID, and(eq(LumeerConst.Document.ID, new ObjectId(documentId)), eq(LumeerConst.Document.METADATA_VERSION_KEY, version))));
+   public DataFilter documentNestedIdFilterWithVersion(final String documentId, final int version) {
+      return createFilter(eq(LumeerConst.Document.ID, and(eq(LumeerConst.Document.ID, new ObjectId(documentId)), eq(LumeerConst.Document.METADATA_VERSION_KEY, version))));
    }
 
    @Override
-   public String documentIdFilter(final String documentId) {
+   public DataFilter documentIdFilter(final String documentId) {
       return fieldValueFilter(LumeerConst.Document.ID, new ObjectId(documentId));
    }
 
    @Override
-   public String multipleFieldsValueFilter(final Map<String, Object> fields) {
+   public DataFilter multipleFieldsValueFilter(final Map<String, Object> fields) {
       List<Bson> bsons = new ArrayList<>();
       fields.entrySet().forEach(e -> bsons.add(eq(e.getKey(), e.getValue())));
-      return convertBsonToJson(and(bsons));
+      return createFilter(and(bsons));
    }
 
    @Override
