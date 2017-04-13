6d1146f6436a87b46bf805c06864dbe708c6c268
Martin Večeřa <marvenec@gmail.com>
2017-04-13 23:59:16 +0200

    added encode and decode to constraint manager

--- war/src/test/java/io/lumeer/engine/rest/DocumentServiceIntegrationTest.java
+++ war/src/test/java/io/lumeer/engine/rest/DocumentServiceIntegrationTest.java
@@ -44,6 +44,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
