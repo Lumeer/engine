0310a3bb848a3174309b605194293472da09a481
Tomáš Livora <livoratom@gmail.com>
2017-07-17 13:54:44 +0200

    Prepare organization permissions tests #LMR-249

---
+++ war/src/test/java/io/lumeer/engine/rest/permissions/OrganizationServicePermissionsIntegrationTest.java
@@ -0,0 +1,68 @@
+/*
+ * -----------------------------------------------------------------------\
+ * Lumeer
+ *  
+ * Copyright (C) since 2016 the original author or authors.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -----------------------------------------------------------------------/
+ */
+package io.lumeer.engine.rest.permissions;
+
+import io.lumeer.engine.IntegrationTestBase;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@Ignore // TODO remove this and implement test scenarios
+@RunWith(Arquillian.class)
+public class OrganizationServicePermissionsIntegrationTest extends IntegrationTestBase {
+
+   @Test
+   public void testGetOrganizationNoRole() {
+
+   }
+
+   @Test
+   public void testGetOrganizationReadRole() {
+
+   }
+
+   @Test
+   public void testGetOrganizationsSomeReadRoles() {
+
+   }
+
+   @Test
+   public void testUpdateOrganizationNoRole() {
+
+   }
+
+   @Test
+   public void testUpdateOrganizationManageRole() {
+
+   }
+
+   @Test
+   public void testCreateProjectInOrganizationNoRole() {
+
+   }
+
+   @Test
+   public void testCreateProjectInOrganizationWriteRole() {
+
+   }
+
+}
