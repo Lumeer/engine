f1ee11ffb27aec3868d9743c2fcf6974bd4e9665
Martin Večeřa <marvenec@gmail.com>
2018-05-05 15:30:12 +0200

    Fix compilation errors in tests

--- war/src/test/java/io/lumeer/core/facade/PaymentFacadeIT.java
+++ war/src/test/java/io/lumeer/core/facade/PaymentFacadeIT.java
@@ -92,7 +92,7 @@ public class PaymentFacadeIT extends IntegrationTestBase {
             .as("With unpaid payment, we should still be on the FREE tier and also after the terms.")
             .isEqualTo(Payment.ServiceLevel.FREE);
 
-      paymentFacade.updatePayment(organization, payment.getId()); // now set it to paid
+      paymentFacade.updatePayment(organization.getCode(), payment.getId()); // now set it to paid
 
       limits = paymentFacade.getServiceLimitsAt(organization, getDate("2011-04-15T12:00:00.000+0100"));
       assertThat(limits.getServiceLevel())
@@ -195,7 +195,7 @@ public class PaymentFacadeIT extends IntegrationTestBase {
       final Payment storedPayment = paymentFacade.createPayment(organization, payment, "", "");
 
       if (paid) {
-         paymentFacade.updatePayment(organization, storedPayment.getId()); //this switches it to PAID in dry run mode
+         paymentFacade.updatePayment(organization.getCode(), storedPayment.getId()); //this switches it to PAID in dry run mode
       }
 
       return storedPayment;
