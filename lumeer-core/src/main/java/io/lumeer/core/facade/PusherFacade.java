68a1063772716ce35c7dd64fb841e7d1b6a8e3f3
Martin Večeřa <marvec@users.noreply.github.com>
2018-12-24 09:45:32 +0100

    Prepare background task execution with spanning the context needed to use dao objects (#250)

--- lumeer-core/src/main/java/io/lumeer/core/facade/PusherFacade.java
+++ lumeer-core/src/main/java/io/lumeer/core/facade/PusherFacade.java
@@ -34,6 +34,7 @@ import io.lumeer.core.auth.AuthenticatedUser;
 import io.lumeer.core.auth.PermissionsChecker;
 import io.lumeer.core.auth.RequestDataKeeper;
 import io.lumeer.core.facade.configuration.DefaultConfigurationProducer;
+import io.lumeer.core.util.PusherClient;
 import io.lumeer.core.util.ResourceUtils;
 import io.lumeer.engine.api.event.CreateDocument;
 import io.lumeer.engine.api.event.CreateLinkInstance;
@@ -57,16 +58,8 @@ import io.lumeer.storage.api.dao.LinkTypeDao;
 import io.lumeer.storage.api.dao.ViewDao;
 import io.lumeer.storage.api.exception.ResourceNotFoundException;
 
-import com.fasterxml.jackson.databind.AnnotationIntrospector;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
-import com.fasterxml.jackson.databind.type.TypeFactory;
-import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
-import org.marvec.pusher.Pusher;
 import org.marvec.pusher.data.Event;
 
-import java.io.IOException;
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -99,9 +92,7 @@ public class PusherFacade {
    private String PUSHER_SECRET;
    private String PUSHER_CLUSTER;
 
-   private Pusher pusher = null;
-
-   private ObjectMapper mapper;
+   private PusherClient pusherClient = null;
 
    @Inject
    private Logger log;
@@ -147,25 +138,7 @@ public class PusherFacade {
       PUSHER_CLUSTER = Optional.ofNullable(defaultConfigurationProducer.get(DefaultConfigurationProducer.PUSHER_CLUSTER)).orElse("");
 
       if (PUSHER_SECRET != null && !"".equals(PUSHER_SECRET)) {
-         pusher = new Pusher(PUSHER_APP_ID, PUSHER_KEY, PUSHER_SECRET);
-         pusher.setCluster(PUSHER_CLUSTER);
-         pusher.setEncrypted(true);
-
-         mapper = new ObjectMapper();
-         AnnotationIntrospector primary = new JacksonAnnotationIntrospector();
-         AnnotationIntrospector secondary = new JaxbAnnotationIntrospector(TypeFactory.defaultInstance());
-         AnnotationIntrospector pair = AnnotationIntrospector.pair(primary, secondary);
-         mapper.setAnnotationIntrospector(pair);
-
-         pusher.setDataMarshaller(o -> {
-            StringWriter sw = new StringWriter();
-            try {
-               mapper.writeValue(sw, o);
-               return sw.toString();
-            } catch (IOException e) {
-               return null;
-            }
-         });
+         pusherClient = new PusherClient(PUSHER_APP_ID, PUSHER_KEY, PUSHER_SECRET, PUSHER_CLUSTER);
       }
    }
 
@@ -183,6 +156,10 @@ public class PusherFacade {
 
    public String getPusherCluster() {
       return PUSHER_CLUSTER;
+   }
+
+   public PusherClient getPusherClient() {
+      return pusherClient;
    }
 
    public void createResource(@Observes final CreateResource createResource) {
@@ -703,18 +680,18 @@ public class PusherFacade {
 
    private void sendNotification(final String userId, final String event, final Object message) {
       if (isEnabled() && authenticatedUser.getCurrentUserId() != null && !authenticatedUser.getCurrentUserId().equals(userId)) {
-         pusher.trigger(PRIVATE_CHANNEL_PREFIX + userId, message.getClass().getSimpleName() + event, message);
+         pusherClient.trigger(PRIVATE_CHANNEL_PREFIX + userId, message.getClass().getSimpleName() + event, message);
       }
    }
 
    private void sendNotificationsBatch(List<Event> notifications) {
       if (isEnabled() && notifications != null && notifications.size() > 0) {
-         pusher.trigger(notifications);
+         pusherClient.trigger(notifications);
       }
    }
 
    private boolean isEnabled() {
-      return pusher != null;
+      return pusherClient != null;
    }
 
    private <T extends Resource> T filterUserRoles(final String userId, final T resource) {
---
+++ lumeer-storage/lumeer-storage-mongodb/src/main/java/io/lumeer/storage/mongodb/dao/context/MongoDaoContextSnapshotFactory.java
@@ -0,0 +1,52 @@
+/*
+ * Lumeer: Modern Data Definition and Processing Platform
+ *
+ * Copyright (C) since 2017 Answer Institute, s.r.o. and/or its affiliates.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.lumeer.storage.mongodb.dao.context;
+
+import io.lumeer.api.SelectedWorkspace;
+import io.lumeer.engine.annotation.SystemDataStorage;
+import io.lumeer.engine.annotation.UserDataStorage;
+import io.lumeer.engine.api.data.DataStorage;
+import io.lumeer.storage.api.dao.context.DaoContextSnapshot;
+import io.lumeer.storage.api.dao.context.DaoContextSnapshotFactory;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+/**
+ * @author <a href="mailto:marvenec@gmail.com">Martin Večeřa</a>
+ */
+@RequestScoped
+public class MongoDaoContextSnapshotFactory implements DaoContextSnapshotFactory {
+
+   @UserDataStorage
+   @Inject
+   private DataStorage dataStorage;
+
+   @SystemDataStorage
+   @Inject
+   private  DataStorage systemDataStorage;
+
+   @Inject
+   private SelectedWorkspace selectedWorkspace;
+
+   @Override
+   public DaoContextSnapshot getInstance() {
+      return new MongoDaoContextSnapshot(systemDataStorage, dataStorage, selectedWorkspace);
+   }
+}
