5cc9096a2d35c37dda3b918daaddda8f29c8b89a
Martin Večeřa <marvenec@gmail.com>
2019-04-15 00:02:20 +0200

    Fix admin email

--- lumeer-core/src/main/java/io/lumeer/core/facade/OrganizationFacade.java
+++ lumeer-core/src/main/java/io/lumeer/core/facade/OrganizationFacade.java
@@ -220,7 +220,7 @@ public class OrganizationFacade extends AbstractFacade {
 
    private void checkSystemPermission() {
       String currentUserEmail = authenticatedUser.getUserEmail();
-      List<String> allowedEmails = Arrays.asList("support@lumeer.io", "martin@vecerovi.com", "kubedo8@gmail.com", "livoratom@gmail.com", "aturing@lumeer.io");
+      List<String> allowedEmails = Arrays.asList("support@lumeer.io", "mvecera@lumeer.io", "kubedo8@gmail.com", "livoratom@gmail.com", "aturing@lumeer.io");
       if (!allowedEmails.contains(currentUserEmail)) {
          throw new NoSystemPermissionException();
       }
