df5a13249775ed64df51e824e3943219aac33e79
Jakub Rodák <kubedo8@gmail.com>
2020-03-28 14:08:21 +0100

    Added option to create template from local resources (#520)

--- war/src/test/java/io/lumeer/core/facade/CopyFacadeIT.java
+++ war/src/test/java/io/lumeer/core/facade/CopyFacadeIT.java
@@ -20,17 +20,23 @@ package io.lumeer.core.facade;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.lumeer.api.model.Attribute;
 import io.lumeer.api.model.Collection;
+import io.lumeer.api.model.ConstraintType;
 import io.lumeer.api.model.Language;
+import io.lumeer.api.model.Document;
 import io.lumeer.api.model.Organization;
 import io.lumeer.api.model.Permission;
 import io.lumeer.api.model.Permissions;
 import io.lumeer.api.model.Project;
+import io.lumeer.api.model.Query;
+import io.lumeer.api.model.QueryStem;
 import io.lumeer.api.model.Role;
 import io.lumeer.api.model.User;
 import io.lumeer.core.WorkspaceKeeper;
 import io.lumeer.core.auth.AuthenticatedUser;
 import io.lumeer.core.template.type.Template;
+import io.lumeer.core.template.type.TemplateType;
 import io.lumeer.engine.IntegrationTestBase;
 import io.lumeer.storage.api.dao.CollectionDao;
 import io.lumeer.storage.api.dao.OrganizationDao;
@@ -42,6 +48,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 
@@ -52,7 +62,7 @@ public class CopyFacadeIT extends IntegrationTestBase {
    private static final String PROJECT_CODE = "TEMPL";
    private static final String USER = AuthenticatedUser.DEFAULT_EMAIL;
 
-   private static final String TEMPLATE = "SUPPLY";
+   private static final TemplateType TEMPLATE = TemplateType.SUPPLY;
    private static final Language language = Language.EN;
 
    @Inject
@@ -75,6 +85,18 @@ public class CopyFacadeIT extends IntegrationTestBase {
 
    @Inject
    private CopyFacade copyFacade;
+
+   @Inject
+   private TemplateFacade templateFacade;
+
+   @Inject
+   private ViewFacade viewFacade;
+
+   @Inject
+   private LinkTypeFacade linkTypeFacade;
+
+   @Inject
+   private SearchFacade searchFacade;
 
    private User user;
    private Project project;
@@ -123,7 +145,7 @@ public class CopyFacadeIT extends IntegrationTestBase {
       projectDao.createProject(project);
 
       for (int i = 0; i < 4; i++) {
-         var collection = new Collection(TEMPLATE + i, TEMPLATE + i, "", "", new Permissions());
+         var collection = new Collection(TEMPLATE.toString() + i, TEMPLATE.toString() + i, "", "", new Permissions());
          collectionDao.setProject(project);
          collectionDao.createCollection(collection);
       }
@@ -131,10 +153,114 @@ public class CopyFacadeIT extends IntegrationTestBase {
 
    @Test
    public void testTemplateImport() {
-      copyFacade.deepCopyTemplate(project, TEMPLATE, language);
+      copyFacade.deepCopyTemplate(project, TEMPLATE.toString(), language);
 
       var collections = collectionFacade.getCollections();
-      var templateCollections = collections.stream().filter(collection -> collection.getName().startsWith(TEMPLATE));
+      var templateCollections = collections.stream().filter(collection -> collection.getName().startsWith(TEMPLATE.toString()));
       assertThat(templateCollections).hasSize(4);
+   }
+
+   @Test
+   @SuppressWarnings("unchecked")
+   public void testTemplateImportLocal() {
+      templateFacade.installTemplate(project, TemplateType.OKR, Language.EN);
+
+      var collections = collectionFacade.getCollections();
+      assertThat(collections).hasSize(4);
+
+      var objectivesCollection = collections.stream().filter(collection -> collection.getName().equals("Objectives")).findFirst().orElse(null);
+      assertThat(objectivesCollection).isNotNull();
+      assertThat(objectivesCollection.getIcon()).isEqualTo("fas fa-crosshairs");
+      assertThat(objectivesCollection.getColor()).isEqualTo("#cc0000");
+      assertThat(objectivesCollection.getAttributes()).hasSize(3);
+      assertThat(objectivesCollection.getDefaultAttributeId()).isEqualTo("a1");
+
+      var a3 = getAttribute(objectivesCollection.getAttributes(), "a3");
+      assertThat(a3).isNotNull();
+      assertThat(a3.getName()).isEqualTo("Progress");
+      assertThat(a3.getConstraint().getType()).isEqualTo(ConstraintType.Percentage);
+      assertThat(a3.getConstraint().getConfig()).isInstanceOf(org.bson.Document.class);
+      assertThat(((org.bson.Document) a3.getConstraint().getConfig()).getLong("decimals")).isEqualTo(0L);
+      assertThat(a3.getFunction()).isNotNull();
+
+      var linkTypes = linkTypeFacade.getLinkTypes();
+      assertThat(linkTypes).hasSize(3);
+      var objectiveKeyResultsLinkType = linkTypes.stream().filter(linkType -> linkType.getName().equals("Objectives Key Results")).findFirst().orElse(null);
+      assertThat(objectiveKeyResultsLinkType).isNotNull();
+
+      assertThat(a3.getFunction().getJs().indexOf("getLinkedDocuments(thisDocument, '" + objectiveKeyResultsLinkType.getId() + "')")).isGreaterThanOrEqualTo(0);
+
+      var employeesCollection = collections.stream().filter(collection -> collection.getName().equals("Employees")).findFirst().orElse(null);
+      assertThat(employeesCollection).isNotNull();
+
+      var allEmployees = searchFacade.searchDocuments(new Query(new QueryStem(employeesCollection.getId())));
+
+      // verify documents hierarchy
+      var natosha = allEmployees.stream().filter(doc -> doc.getData().getString("a1").equals("Natosha")).findFirst().orElse(null);
+      assertThat(natosha).isNotNull();
+      var parent1 = natosha.getMetaData().getString(Document.META_PARENT_ID);
+      assertThat(parent1).isNotNull();
+      var jasmin = allEmployees.stream().filter(doc -> doc.getData().getId().equals(parent1)).findFirst().orElse(null);
+      assertThat(jasmin).isNotNull();
+      assertThat(jasmin.getData()).contains(Map.entry("a1", "Jasmin"));
+      var parent2 = jasmin.getMetaData().getString(Document.META_PARENT_ID);
+      assertThat(parent2).isNotNull();
+      var marta = allEmployees.stream().filter(doc -> doc.getData().getId().equals(parent2)).findFirst().orElse(null);
+      assertThat(marta).isNotNull();
+      assertThat(marta.getData()).contains(Map.entry("a1", "Marta"));
+      var parent3 = marta.getMetaData().getString(Document.META_PARENT_ID);
+      assertThat(parent3).isNotNull();
+      var macie = allEmployees.stream().filter(doc -> doc.getData().getId().equals(parent3)).findFirst().orElse(null);
+      assertThat(macie).isNotNull();
+      assertThat(macie.getData()).contains(Map.entry("a1", "Macie"));
+
+      var keyResultsCollection = collections.stream().filter(collection -> collection.getName().equals("Key Results")).findFirst().orElse(null);
+      var initiativesCollection = collections.stream().filter(collection -> collection.getName().equals("Initiatives")).findFirst().orElse(null);
+      assertThat(keyResultsCollection).isNotNull();
+      assertThat(initiativesCollection).isNotNull();
+      assertThat(objectiveKeyResultsLinkType.getCollectionIds()).containsExactly(objectivesCollection.getId(), keyResultsCollection.getId());
+
+      var allKeyResults = searchFacade.searchDocuments(new Query(new QueryStem(keyResultsCollection.getId())));
+      var allInitiatives = searchFacade.searchDocuments(new Query(new QueryStem(initiativesCollection.getId())));
+
+      var conversionDoc = allKeyResults.stream().filter(doc -> doc.getData().getString("a1").equals("15% conversion")).findFirst().orElse(null);
+      var abTestingDoc = allInitiatives.stream().filter(doc -> doc.getData().getString("a1").equals("A/B testing the CTA")).findFirst().orElse(null);
+      assertThat(conversionDoc).isNotNull();
+      assertThat(abTestingDoc).isNotNull();
+
+      var keyResultsInitiativesLinkType = linkTypes.stream().filter(linkType -> linkType.getName().equals("Key Results Initiatives")).findFirst().orElse(null);
+      assertThat(keyResultsInitiativesLinkType).isNotNull();
+
+      var links = searchFacade.getLinkInstances(new Query(new QueryStem(keyResultsCollection.getId(), List.of(keyResultsInitiativesLinkType.getId()), Set.of(conversionDoc.getId()), Collections.emptySet(), Collections.emptySet())));
+      assertThat(links).isNotNull();
+
+      var allDocuments = searchFacade.searchDocuments(new Query());
+      assertThat(allDocuments).hasSize(78);
+
+      var views = viewFacade.getViews();
+      assertThat(views).hasSize(6);
+
+      var okrInitiatives = views.stream().filter(view -> view.getName().equals("OKR Initiatives")).findFirst().orElse(null);
+      assertThat(okrInitiatives).isNotNull();
+      org.bson.Document config = (org.bson.Document) okrInitiatives.getConfig();
+      assertThat(config.containsKey("table")).isTrue();
+      var table = (org.bson.Document) config.get("table");
+      assertThat(table.containsKey("parts"));
+      var parts = (List<org.bson.Document>) table.get("parts");
+      var collectionConfig = parts.stream().filter(doc -> doc.containsKey("collectionId") && doc.getString("collectionId").equals(keyResultsCollection.getId())).findFirst().orElse(null);
+      assertThat(collectionConfig).isNotNull();
+      var columns = (List<org.bson.Document>) collectionConfig.get("columns");
+      assertThat(columns).isNotNull();
+      var hiddenPart = columns.stream().filter(doc -> doc.containsKey("type") && doc.getString("type").equals("hidden")).findFirst().orElse(null);
+      assertThat(hiddenPart).isNotNull();
+      assertThat(hiddenPart.containsKey("attributeIds")).isTrue();
+      assertThat((List<String>) hiddenPart.get("attributeIds")).containsExactly("a3", "a4", "a5", "a2");
+
+      assertThat(okrInitiatives.getQuery().getStems()).hasSize(1);
+      assertThat(okrInitiatives.getQuery().getStems().get(0).getCollectionId()).isEqualTo(keyResultsCollection.getId());
+   }
+
+   private Attribute getAttribute(final java.util.Collection<Attribute> attributes, final String id) {
+      return attributes.stream().filter(attribute -> attribute.getId().equals(id)).findFirst().orElse(null);
    }
 }
