c0c74b2c55f84a470090b3eb592a8876db6534d2
Martin Večeřa <marvec@users.noreply.github.com>
2021-02-13 15:51:46 +0100

    Add caching to filters (#715)
    * Add caching to filters
    
    * Add lazy initialization to proxy list

--- lumeer-core/src/main/kotlin/io/lumeer/core/util/js/JvmListProxy.kt
+++ lumeer-core/src/main/kotlin/io/lumeer/core/util/js/JvmListProxy.kt
@@ -24,22 +24,30 @@ import java.util.*
 
 class JvmListProxy(val values: MutableList<Any?>, val locale: Locale = Locale.getDefault()) : ProxyArray {
 
+    private val objects: Array<Any?> = Array(values.size) { null }
+
     override fun get(index: Long): Any? {
         checkIndex(index)
-        return if (values.getOrNull(index.toInt()) != null) JvmObjectProxy.encodeObject(values[index.toInt()]!!, locale)
-        else null
+        val i = index.toInt()
+
+        val o = objects[i]
+        if (o != null) {
+            return o
+        }
+
+        val v = values[i]
+        if (v != null) {
+            val enc = JvmObjectProxy.encodeObject(v, locale)
+            objects[i] = enc
+            return enc
+        }
+
+        return null
     }
 
-    override fun set(index: Long, value: Value) {
-        checkIndex(index)
-        values[index.toInt()] = if (value.isHostObject) value.asHostObject<Any>() else value
-    }
+    override fun set(index: Long, value: Value) = throw UnsupportedOperationException()
 
-    override fun remove(index: Long): Boolean {
-        checkIndex(index)
-        values.removeAt(index.toInt())
-        return true
-    }
+    override fun remove(index: Long): Boolean = throw UnsupportedOperationException()
 
     private fun checkIndex(index: Long) {
         if (index > 2147483647L || index < 0L) {
