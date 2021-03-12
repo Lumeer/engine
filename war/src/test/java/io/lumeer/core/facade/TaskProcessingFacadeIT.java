1ce49e1a21313d3db644a9c4450bbea8212051dc
Martin Večeřa <marvenec@gmail.com>
2021-03-12 11:55:18 +0100

    Fix endless loop test

--- war/src/test/java/io/lumeer/core/facade/TaskProcessingFacadeIT.java
+++ war/src/test/java/io/lumeer/core/facade/TaskProcessingFacadeIT.java
@@ -309,7 +309,9 @@ public class TaskProcessingFacadeIT extends IntegrationTestBase {
       updatedCollection = collectionFacade.getCollection(c1.getId());
       updatedRule = new BlocklyRule(updatedCollection.getRules().get(ruleName));
 
-      assertThat(updatedRule.getError()).contains("Execution got cancelled");
+      final String error = updatedRule.getError();
+      assertThat(error.contains("Execution got cancelled") || error.contains("Thread was interrupted")).isTrue();
+
       // it should have been interrupted after 3000ms
       assertThat(System.currentTimeMillis() - updatedRule.getResultTimestamp()).isLessThan(5000);
    }
