51630f78b52e122919ff7d7cb13415d3e557159b
Martin Večeřa <marvenec@gmail.com>
2021-10-09 01:06:22 +0200

    Fix import

--- lumeer-core/src/main/java/io/lumeer/core/util/SelectionListUtils.java
+++ lumeer-core/src/main/java/io/lumeer/core/util/SelectionListUtils.java
@@ -1,10 +1,10 @@
 package io.lumeer.core.util;
 
+import io.lumeer.api.model.SelectOption;
 import io.lumeer.api.model.SelectionList;
 
-import org.assertj.core.util.Lists;
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SelectionListUtils {
@@ -19,7 +19,14 @@ public class SelectionListUtils {
    }
 
    public static List<SelectionList> getPredefinedLists() {
+      /*
+      Normal - Open, In Progress, Closed
+      Kanban - Open, In Progress, Review, Closed
+      Content - Open, Ready, Writing, Approval, Rejected, Publish, Closed
+      Marketing - Open, Concept, In Progress, Running, Review, Closed
+      Scrum - Open, Pending, In Progress, Completed, In Review, Accepted, Rejected, Blocked, Closed
+      */
       // TODO;
-      return Lists.emptyList();
+      return Collections.emptyList();
    }
 }
