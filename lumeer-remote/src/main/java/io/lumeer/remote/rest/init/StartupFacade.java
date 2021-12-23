5d9803dbb869a0b11bfdda7a08a720e6830045b4
Martin Večeřa <marvec@users.noreply.github.com>
2021-12-23 11:50:12 +0100

    Clear database migration from the startup facade (#889)

--- lumeer-remote/src/main/java/io/lumeer/remote/rest/init/StartupFacade.java
+++ lumeer-remote/src/main/java/io/lumeer/remote/rest/init/StartupFacade.java
@@ -18,31 +18,14 @@
  */
 package io.lumeer.remote.rest.init;
 
-import io.lumeer.api.model.Attribute;
-import io.lumeer.api.model.AttributeFilterEquation;
-import io.lumeer.api.model.AttributeLock;
-import io.lumeer.api.model.AttributeLockExceptionGroup;
-import io.lumeer.api.model.Constraint;
-import io.lumeer.api.model.ConstraintType;
-import io.lumeer.api.model.LinkType;
-import io.lumeer.api.util.AttributeUtil;
 import io.lumeer.core.WorkspaceKeeper;
 import io.lumeer.storage.api.dao.CollectionDao;
 import io.lumeer.storage.api.dao.LinkTypeDao;
 import io.lumeer.storage.api.dao.OrganizationDao;
 import io.lumeer.storage.api.dao.ProjectDao;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-
-import java.io.IOException;
 import java.io.Serializable;
-import java.util.Collections;
-import java.util.Map;
-import java.util.concurrent.atomic.LongAdder;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -70,7 +53,7 @@ public class StartupFacade implements Serializable {
 
    @PostConstruct
    public void afterDeployment() {
-      log.info("Checking database for updates...");
+      /*log.info("Checking database for updates...");
       long tm = System.currentTimeMillis();
 
       final LongAdder orgs = new LongAdder(), projs = new LongAdder(), attributes = new LongAdder();
@@ -92,28 +75,6 @@ public class StartupFacade implements Serializable {
                linkTypeDao.setProject(project);
                collectionDao.setProject(project);
 
-               collectionDao.getAllCollections().forEach(collection -> {
-                  log.info("Read " + collection.getAttributes().size() + " collection attributes.");
-
-                  var originalCollection = collection.copy();
-
-                  var migratedAttributes = collection.getAttributes().stream().map(attribute -> this.migrateAttribute(attribute, attributes)).collect(Collectors.toSet());
-                  collection.setAttributes(migratedAttributes);
-
-                  collectionDao.updateCollection(collection.getId(), collection, originalCollection, false);
-               });
-
-               linkTypeDao.getAllLinkTypes().forEach(linkType -> {
-                  log.info("Read " + linkType.getAttributes().size() + " collection attributes.");
-
-                  var originalLinkType = new LinkType(linkType);
-
-                  var migratedAttributes = linkType.getAttributes().stream().map(attribute -> this.migrateAttribute(attribute, attributes)).collect(Collectors.toSet());
-                  linkType.setAttributes(migratedAttributes);
-
-                  linkTypeDao.updateLinkType(linkType.getId(), linkType, originalLinkType, false);
-               });
-
             });
          });
 
@@ -126,34 +87,7 @@ public class StartupFacade implements Serializable {
       log.info(String.format("Updated %d organizations, %d project, %d attributes.",
             orgs.longValue(), projs.longValue(), attributes.longValue()));
 
-      log.info("Updates completed in " + (System.currentTimeMillis() - tm) + "ms.");
+      log.info("Updates completed in " + (System.currentTimeMillis() - tm) + "ms.");*/
    }
 
-   private Attribute migrateAttribute(Attribute attribute, LongAdder adder) {
-      Constraint constraint = attribute.getConstraint();
-      AttributeLock attributeLock = null;
-      if (constraint != null && constraint.getType() == ConstraintType.Action && AttributeUtil.isConstraintWithConfig(attribute)) {
-         @SuppressWarnings("unchecked") final Map<String, Object> config = (Map<String, Object>) constraint.getConfig();
-         config.remove("role");
-         Object equationObject = config.remove("equation");
-         if (equationObject != null) {
-            ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-            try {
-               String json = ow.writeValueAsString(equationObject);
-               ObjectMapper objectMapper = new ObjectMapper();
-               AttributeFilterEquation equation = objectMapper.readValue(json, AttributeFilterEquation.class);
-               AttributeLockExceptionGroup group = new AttributeLockExceptionGroup(equation, Collections.emptyList(), "everyone");
-               attributeLock = new AttributeLock(Collections.singletonList(group), true);
-               adder.increment();
-            } catch (IOException ignored) {
-
-            }
-         }
-      } else if (AttributeUtil.functionIsDefined(attribute)) {
-         attributeLock = new AttributeLock(Collections.emptyList(), !attribute.getFunction().isEditable());
-         adder.increment();
-      }
-
-      return new Attribute(attribute.getId(), attribute.getName(), attribute.getDescription(), constraint, attributeLock, attribute.getFunction(), attribute.getUsageCount());
-   }
 }
