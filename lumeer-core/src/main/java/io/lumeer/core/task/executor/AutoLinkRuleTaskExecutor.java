13ad2d3573c81ca0a25fe8dd5622d60d5ab3e6b7
Martin Večeřa <marvenec@gmail.com>
2022-04-21 20:02:48 +0200

    Update also calls on removed links

--- lumeer-core/src/main/java/io/lumeer/core/task/executor/AutoLinkRuleTaskExecutor.java
+++ lumeer-core/src/main/java/io/lumeer/core/task/executor/AutoLinkRuleTaskExecutor.java
@@ -134,9 +134,8 @@ public class AutoLinkRuleTaskExecutor {
             });
 
             final FunctionFacade functionFacade = ruleTask.getFunctionFacade();
-            final List<String> skipCollectionIds = List.of(thisCollection);
             linksForRemoval.forEach(linkInstance -> {
-               taskExecutor.submitTask(functionFacade.createTaskForRemovedLinks(matcher.getLinkType(), Collections.singletonList(linkInstance), skipCollectionIds));
+               taskExecutor.submitTask(functionFacade.createTaskForRemovedLinks(matcher.getLinkType(), Collections.singletonList(linkInstance)));
             });
          }
 
