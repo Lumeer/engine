f2bee6de10a26eda0226938d2a1264089c7ffb03
Jakub <kubedo8@gmail.com>
2022-08-15 23:55:33 +0200

    Fix getting public view by code

--- lumeer-core/src/main/java/io/lumeer/core/auth/Auth0Filter.java
+++ lumeer-core/src/main/java/io/lumeer/core/auth/Auth0Filter.java
@@ -56,7 +56,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
