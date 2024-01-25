7c91b11d5c4d97a7215c715ff4d438ebacab41b9
Martin Večeřa <marvenec@gmail.com>
2024-01-25 23:25:50 +0100

    Migrate to the new MailerLite API

--- lumeer-core/src/main/java/io/lumeer/core/facade/MailerLiteFacade.java
+++ lumeer-core/src/main/java/io/lumeer/core/facade/MailerLiteFacade.java
@@ -22,14 +22,20 @@ import io.lumeer.api.model.User;
 import io.lumeer.core.facade.configuration.DefaultConfigurationProducer;
 
 import org.apache.commons.lang3.StringUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -54,29 +60,58 @@ public class MailerLiteFacade implements MailerService {
    private static String MAILERLITE_APIKEY;
    private static String MAILERLITE_GROUP_CS;
    private static String MAILERLITE_GROUP_EN;
-   private static String MAILERLITE_SEQUENCE_CS;
-   private static String MAILERLITE_SEQUENCE_EN;
+   private static String MAILERLITE_NEWSLETTER_CS;
+   private static String MAILERLITE_NEWSLETTER_EN;
 
    @PostConstruct
    public void init() {
       MAILERLITE_APIKEY = Optional.ofNullable(defaultConfigurationProducer.get(DefaultConfigurationProducer.MAILERLITE_APIKEY)).orElse("");
       MAILERLITE_GROUP_CS = Optional.ofNullable(defaultConfigurationProducer.get(DefaultConfigurationProducer.MAILERLITE_GROUP_CS)).orElse("");
       MAILERLITE_GROUP_EN = Optional.ofNullable(defaultConfigurationProducer.get(DefaultConfigurationProducer.MAILERLITE_GROUP_EN)).orElse("");
-      MAILERLITE_SEQUENCE_CS = Optional.ofNullable(defaultConfigurationProducer.get(DefaultConfigurationProducer.MAILERLITE_SEQUENCE_CS)).orElse("");
-      MAILERLITE_SEQUENCE_EN = Optional.ofNullable(defaultConfigurationProducer.get(DefaultConfigurationProducer.MAILERLITE_SEQUENCE_EN)).orElse("");
+      MAILERLITE_NEWSLETTER_CS = Optional.ofNullable(defaultConfigurationProducer.get(DefaultConfigurationProducer.MAILERLITE_NEWSLETTER_CS)).orElse("");
+      MAILERLITE_NEWSLETTER_EN = Optional.ofNullable(defaultConfigurationProducer.get(DefaultConfigurationProducer.MAILERLITE_NEWSLETTER_EN)).orElse("");
    }
 
    @Override
    public void setUserSubscription(final User user, final boolean enSite) {
       if (StringUtils.isNotEmpty(MAILERLITE_APIKEY) && user != null && user.getEmail() != null) {
 
-         if (!userSubscribed(user.getEmail())) {
-            updateUser(user.getEmail(), true);
+         final String welcomeGroup = enSite ? MAILERLITE_GROUP_EN : MAILERLITE_GROUP_CS;
+         final String newsletterGroup = enSite ? MAILERLITE_NEWSLETTER_EN : MAILERLITE_NEWSLETTER_CS;
 
-            subscribeUser(enSite ? MAILERLITE_SEQUENCE_EN : MAILERLITE_SEQUENCE_CS, user);
-            subscribeUser(enSite ? MAILERLITE_GROUP_EN : MAILERLITE_GROUP_CS, user.getName(), user.getEmail(), true);
+         final SubscribedUser subscribedUser = userSubscribed(user.getEmail());
+         final String fields = "\"goto_news\": \"" + (user.hasNewsletter() != null && user.hasNewsletter() ? "news" : "no") + "\"";
+
+         if (subscribedUser == null) { // new user is getting subscribed
+            // user is subscribed as active
+            // user is added to app welcome sequence
+            // goto_news filed is set to "news" if newsletter is opted in
+            // user is added to newsletter group by a MailerLite automation once they go through the welcome sequence
+
+            final String groups = "\"" + welcomeGroup + "\"";
+
+            upsertUser(user, fields, groups);
          } else {
-            updateUser(user.getEmail(), user.hasNewsletter() != null && user.hasNewsletter());
+            // update newsletter group and goto_news field according to subscription status
+            String groups = "";
+
+            // if the user is subscribed to the newsletter but is still in the welcome sequence, do nothing with the groups
+            // if the user is subscribed to the newsletter and is not in the welcome sequence, add them to the newsletter group
+            if (user.hasNewsletter() != null && user.hasNewsletter()) {
+               if (!subscribedUser.groups.contains(welcomeGroup)) {
+                  groups = "\"" + newsletterGroup + "\"";
+               }
+            }
+
+            // if the user is in the welcome sequence, keep them there
+            if (subscribedUser.groups.contains(welcomeGroup)) {
+               if (StringUtils.isNotEmpty(groups)) {
+                  groups += ", ";
+               }
+               groups += "\"" + welcomeGroup + "\"";
+            }
+
+            updateUser(subscribedUser.id, user, fields, groups);
          }
       }
    }
@@ -84,7 +119,9 @@ public class MailerLiteFacade implements MailerService {
    @Override
    public void setUserTemplate(final User user, final String template) {
       if (StringUtils.isNotEmpty(MAILERLITE_APIKEY) && user != null && StringUtils.isNotEmpty(user.getEmail())) {
-         setUserTemplate(user.getEmail(), template);
+         final String fields = "\"template\": \"" + encodeValue(template) + "\"";
+
+         upsertUser(user, fields, "");
       }
    }
 
@@ -96,34 +133,43 @@ public class MailerLiteFacade implements MailerService {
       }
    }
 
-   private boolean userSubscribed(final String userEmail) {
+   @SuppressWarnings("unchecked")
+   private SubscribedUser userSubscribed(final String userEmail) {
       String response = mailerLiteClient("subscribers/" + encodeValue(userEmail), null, false, true);
 
-      return response.startsWith("{\"id\":") && response.contains("\"email\":\"" + userEmail + "\"");
+      JSONParser parser = new JSONParser();
+      try {
+         JSONObject jsonObject = (JSONObject) parser.parse(response);
+         if (jsonObject.containsKey("data")) {
+            final String id = (String) ((JSONObject) jsonObject.get("data")).get("id");
+            final List<String> groups = ((Stream<JSONObject>) ((JSONArray) ((JSONObject) jsonObject.get("data")).get("groups")).stream()).map(o -> (String) o.get("id")).collect(Collectors.toList());
+            return new SubscribedUser(id, groups);
+         } else {
+            return null;
+         }
+      } catch (Throwable e) {
+         return null;
+      }
    }
 
-   private void subscribeUser(final String groupId, final User user) {
-      subscribeUser(groupId, user.getName(), user.getEmail(), user.hasNewsletter());
+   private void upsertUser(final User user, final String fields, final String groups) {
+      mailerLiteClient("subscribers", "{\"email\": \"" + user.getEmail() +
+            "\", \"fields\": {\"name\":\"" + user.getName() + "\"" + (StringUtils.isNotEmpty(fields) ? ", " + fields : "") +
+            "}" + (StringUtils.isNotEmpty(groups) ? ", \"groups\": [" + groups + "]" : "") +
+            ", \"status\": \"active\"}", false, false);
    }
 
-   private void subscribeUser(final String groupId, final String name, final String email, final boolean active) {
-      mailerLiteClient("groups/" + groupId + "/subscribers", "{\"email\": \"" + email + "\", \"name\":\"" + name + "\", \"type\": \"" + (active ? "active" : "unsubscribed") +
-            "\"}", false, false);
-   }
-
-   private void updateUser(final String userEmail, boolean subscribed) {
-      mailerLiteClient("subscribers/" + encodeValue(userEmail), "{\"type\": \"" + (subscribed ? "active" : "unsubscribed") + "\"}", true, false);
-   }
-
-   private void setUserTemplate(final String userEmail, final String template) {
-      mailerLiteClient("subscribers/" + encodeValue(userEmail), "{\"fields\": { \"template\": \"" + encodeValue(template) + "\" } }", true, false);
+   private void updateUser(final String userMailreLiteId, final User user, final String fields, final String groups) {
+      mailerLiteClient("subscribers/" + encodeValue(userMailreLiteId), "{\"fields\": {\"name\":\"" + user.getName() + "\"" + (StringUtils.isNotEmpty(fields) ? ", " + fields : "") +
+            "}" + ", \"groups\": [" + groups + "]" +
+            ", \"status\": \"active\"}", true, false);
    }
 
    private String mailerLiteClient(final String path, final String body, final boolean put, final boolean blocking) {
       final Client client = ClientBuilder.newBuilder().build();
-      final Invocation.Builder builder = client.target("https://api.mailerlite.com/api/v2/" + path)
+      final Invocation.Builder builder = client.target("https://connect.mailerlite.com/api/" + path)
                                                .request(MediaType.APPLICATION_JSON)
-                                               .header("X-MailerLite-ApiKey", MAILERLITE_APIKEY)
+                                               .header("Authorization", "Bearer " + MAILERLITE_APIKEY)
                                                .header("Content-Type", "application/json");
       Invocation invocation;
 
@@ -176,4 +222,5 @@ public class MailerLiteFacade implements MailerService {
       }).start();
    }
 
+   public record SubscribedUser(String id, List<String> groups) { }
 }
